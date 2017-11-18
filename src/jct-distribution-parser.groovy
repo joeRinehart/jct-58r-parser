b5d085e170dcc8d2507c5f485ba353456b3c422e
Joe Rinehart <joe.rinehart@gmail.com>
2017-11-18 07:07:54 -0500

    Coffee.

---
+++ src/jct-distribution-parser.groovy
@@ -0,0 +1,92 @@
+File raw = new File('../input/jct-distribution.txt')
+
+Boolean payingAttention = false
+Integer currentYear
+List incomeBrackets = []
+
+Map ignoredStarts = [
+    'CHANGE IN FEDERAL TAXES (3)' : true,
+    'INCOME FEDERAL' : true,
+    'CATEGORY (2) TAXES (3)' : true,
+    'Millions Percent Billions' : true
+]
+
+def fromCurrencyString = { String value, BigInteger extra ->
+    BigDecimal intermediate = ( value.toString().replace('$', '').replaceAll(',', '')) as BigDecimal
+    return intermediate * extra
+}
+def fromMillionsString = { String value ->
+    return fromCurrencyString( value, 1000000)
+}
+def fromBillionsString = { String value ->
+    return fromCurrencyString( value, 1000000000)
+}
+def fromPercentageString = { String value ->
+    return ( (value.toString().replace('%', '').replace('(','').replace(')', '') as BigDecimal)  / 100)
+}
+def tokensToData = { List tokens ->
+    String bracket = tokens.subList(0, 3).join(' ')
+    if ( !incomeBrackets.contains(bracket) ) {
+        incomeBrackets << bracket
+    }
+    return [
+        income_bracket: bracket,
+        federal_tax_change_millions: fromMillionsString( tokens[3] ),
+        federal_tax_change_percent: fromPercentageString( tokens[4] ),
+        taxes_under_current_billions: fromBillionsString( tokens[5] ),
+        taxes_under_current_percent: fromPercentageString( tokens[6] ),
+        taxes_under_proposal_billions: fromBillionsString( tokens[7] ),
+        taxes_under_proposal_percent: fromPercentageString( tokens[8] ),
+        rate_under_current_percent: fromPercentageString( tokens[9] ),
+        rate_under_proposal_percent: fromPercentageString( tokens[10] ),
+    ]
+}
+
+Map statsByYear = [:]
+
+raw.eachLine { line ->
+    line = line.trim()
+    if ( !payingAttention && line.startsWith("Calendar Year") ) {
+        payingAttention = true
+    } else if ( payingAttention && line.startsWith("Source: Joint Committee on Taxation") ) {
+        payingAttention = false
+    }
+
+    if ( payingAttention ) {
+        Boolean include = true
+        // efficiency? it's a small file.
+        ignoredStarts.eachWithIndex{ start, v, i ->
+            if ( include ) {
+                include = !line.startsWith(start)
+            }
+        }
+        if ( include ) {
+            if ( line.startsWith("Calendar Year") ) {
+                currentYear = line.tokenize(' ').last() as Integer
+                statsByYear[ currentYear ] = []
+            } else {
+                statsByYear[currentYear] <<tokensToData( line.tokenize() )
+            }
+        }
+    }
+}
+
+// create simple tab delimit of years (cols) brackets (rows) and proposed tax rates
+print 'Income Bracket\t2017\t'
+statsByYear.eachWithIndex { year, v, i ->
+    print year + '\t'
+}
+print '\n'
+incomeBrackets.each{ bracket ->
+    print bracket + '\t0\t'
+    statsByYear.eachWithIndex{ year, v, i  ->
+        def stat = v.find{
+            it.income_bracket == bracket
+        }
+        print stat.rate_under_proposal_percent - stat.rate_under_current_percent + '\t'
+    }
+    print '\n'
+}
+
+
+
